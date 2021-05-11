e34d966e24ded8e7d23ef3cbb991994e64d8b6c4
gaoyang <2713284331@qq.com>
2021-05-11 16:56:02 +0800

    删除了一行代码

--- hellospringboot/src/test/java/com/itheima/hellospringboot/HellospringbootApplicationTests.java
+++ hellospringboot/src/test/java/com/itheima/hellospringboot/HellospringbootApplicationTests.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+
 
 import java.util.List;
 
